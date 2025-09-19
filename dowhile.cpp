75900c0d8edde67e2135ac2270c590b700940173
Samuel Torres <rilla@MacBook-Pro.local>
2025-09-19 11:09:13 -0400

    Final Commit

--- dowhile.cpp
+++ dowhile.cpp
@@ -14,6 +14,9 @@ int main()
 	// Fill in the code to define an integer variable called number,
 	// a floating point variable called cost,
 	// and a character variable called beverage
+	int number;
+	double cost;
+	char beverage;
 
 	bool validBeverage;
 
@@ -21,30 +24,38 @@ int main()
 
 	do
 	{
-		cout << endl << endl;
-		cout << "Hot Beverage Menu" << endl << endl;
+		cout << endl
+			 << endl;
+		cout << "Hot Beverage Menu" << endl
+			 << endl;
 		cout << "A: Coffee	$1.00" << endl;
 		cout << "B: Tea	$ .75" << endl;
 		cout << "C: Hot Chocolate	$1.25" << endl;
-		cout << "D: Cappuccino	$2.50" << endl << endl << endl;
+		cout << "D: Cappuccino	$2.50" << endl
+			 << endl
+			 << endl;
 
 		cout << "Enter the beverage A,B,C, or D you desire" << endl;
-		cout << "Enter E to exit the program" << endl << endl;
+		cout << "Enter E to exit the program" << endl
+			 << endl;
 
 		// Fill in the code to read in beverage
+		cin >> beverage;
 
 		switch (beverage)
 		{
-			case 'a':
-			case 'A':
-			case 'b':
-			case 'B':
-			case 'c':
-			case 'C':
-			case 'd':
-			case 'D': validBeverage = true;
-				break;
-			default: validBeverage = false;
+		case 'a':
+		case 'A':
+		case 'b':
+		case 'B':
+		case 'c':
+		case 'C':
+		case 'd':
+		case 'D':
+			validBeverage = true;
+			break;
+		default:
+			validBeverage = false;
 		}
 
 		if (validBeverage == true)
@@ -52,32 +63,55 @@ int main()
 			cout << "How many cups would you like?" << endl;
 
 			// Fill in the code to read in number
+			cin >> number;
 		}
 
 		// Fill in the code to begin a switch statement
 		// that is controlled by beverage
+		switch (beverage)
 		{
-			case 'a':
-			case 'A': cost = number * 1.0;
-				cout << "The total cost is $ " << cost << endl;
-				break;
+		case 'a':
+		case 'A':
+			cost = number * 1.0;
+			cout << "The total cost is $ " << cost << endl;
+			break;
 
-				// Fill in the code to give the case for hot chocolate ($1.25 a cup)
+			// Fill in the code to give the case for hot chocolate ($1.25 a cup)
+		case 'c':
+		case 'C':
+			cost = number * 1.25;
+			cout << "The total cost is $ " << cost << endl;
+			break;
 
-				// Fill in the code to give the case for tea ( $0.75 a cup)
+			// Fill in the code to give the case for tea ( $0.75 a cup)
+		case 'b':
+		case 'B':
+			cost = number * 0.75;
+			cout << "The total cost is $ " << cost << endl;
+			break;
 
-				// Fill in the code to give the case for cappuccino ($2.50 a cup)
+			// Fill in the code to give the case for cappuccino ($2.50 a cup)
+		case 'd':
+		case 'D':
+			cost = number * 2.50;
+			cout << "The total cost is $ " << cost << endl;
+			break;
 
-			case 'e':
-			case 'E': cout << " Please come again" << endl;
-				break;
-			default: cout <<	// Fill in the code to write a message
-						// indicating an invalid selection.
-				 cout << " Try again please" << endl;
+		case 'e':
+		case 'E':
+			cout << " Please come again" << endl;
+			break;
+		default:
+			cout << // Fill in the code to write a message
+					// indicating an invalid selection.
+				"Invalid selection." << endl;
+			cout << " Try again please" << endl;
 		}
 
-	}	// Fill in the code to finish the do-while statement with the
-		// condition that beverage does not equal E or e.
+	} // Fill in the code to finish the do-while statement with the
+	  // condition that beverage does not equal E or e.
+	while (beverage != 'E' && beverage != 'e');
 
-		// Fill in the appropriate return statement
+	// Fill in the appropriate return statement
+	return 0;
 }
